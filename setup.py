04f1cd6709c02087d3913216670a08967bd59a42
EliseyGodX <krotov.ruslan.31@gmail.com>
2024-10-02 00:36:46 +0300

    v1.0.0

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='patisson_graphql',
-    version='0.2.0',
+    version='1.0.0',
     packages=find_packages(),
+    install_requires=[
+        "ariadne",
+        "sqlalchemy"
+    ],
     author='EliseyGodX',
     description='Tools for developing microservices using GraphQL (ariadne)',
     long_description=open('README.md').read(),
