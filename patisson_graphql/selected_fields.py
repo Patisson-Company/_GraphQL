1a44ec5c13e200b3a19e3696213e281640e29782
EliseyGodX <krotov.ruslan.31@gmail.com>
2024-12-12 19:50:24 +0300

    v1.1.1

--- patisson_graphql/selected_fields.py
+++ patisson_graphql/selected_fields.py
@@ -9,6 +9,16 @@ from sqlalchemy.orm.attributes import InstrumentedAttribute
 
 
 def selected_fields(info: GraphQLResolveInfo, model) -> list[InstrumentedAttribute]:
+    """
+    Extracts selected fields from a GraphQL query and maps them to the attributes of the given SQLAlchemy model.
+
+    Args:
+        info (GraphQLResolveInfo): The GraphQL query resolution information.
+        model: The SQLAlchemy model to map the fields to.
+
+    Returns:
+        list[InstrumentedAttribute]: A list of SQLAlchemy model attributes corresponding to the selected fields.
+    """
     names: set[str] = set()
     for node in info.field_nodes:
         if node.selection_set is None: continue
@@ -19,6 +29,19 @@ def selected_fields(info: GraphQLResolveInfo, model) -> list[InstrumentedAttribu
 def _fields_from_selections(
     info: GraphQLResolveInfo, selections: Iterable[SelectionNode]
 ) -> list[str]:
+    """
+    Recursively extracts field names from GraphQL selections.
+
+    Args:
+        info (GraphQLResolveInfo): The GraphQL query resolution information.
+        selections (Iterable[SelectionNode]): The selection nodes from the GraphQL query.
+
+    Returns:
+        list[str]: A list of field names extracted from the selections.
+
+    Raises:
+        NotImplementedError: If an unsupported selection type is encountered.
+    """
     names: list[str] = []
     for selection in selections:
         match selection:
